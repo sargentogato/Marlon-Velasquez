29ed6e834eeeeaefa4b22aafdc53760fc9e972e5
Marlon Velasquez <marlonjvelasquez@gmail.com>
2020-08-30 18:09:50 +0200

    Refactoring: -Now the event listener is only applied to the section__box class

--- script/eventListener.js
+++ script/eventListener.js
@@ -1,96 +1,104 @@
-document.addEventListener("mouseover", (event) => {
-  let domElement = event.target.parentElement.id;
+let elementToListen = document.querySelectorAll(".section__box")
+let listenedSections = elementToListen.length
 
-  switch (domElement) {
-    case "boardGame":
-      addClass(
-        domElement,
-        "https://sargentogato.github.io/turn-based-game-in-javaScript/"
-      );
-      break;
+for (const element of elementToListen) {
+  
+  element.addEventListener("mouseover", (event) => {
+    console.log("entra")
+    let domElement = event.target.parentElement.id;
+  
+    switch (domElement) {
+      case "boardGame":
+        addClass(
+          domElement,
+          "https://sargentogato.github.io/turn-based-game-in-javaScript/"
+        );
+        break;
+  
+      case "tetris":
+        addClass(domElement, "https://sargentogato.github.io/Tetris-sg/");
+        break;
+  
+      case "crazyQuote":
+        addClass(
+          domElement,
+          "https://sargentogato.github.io/crazyquotegenerator/"
+        );
+        break;
+  
+      case "js":
+        addClass(
+          domElement,
+          "https://1drv.ms/x/s!AuDwzFWCb33QgrZtIVvj3SoU3cNlRg?e=TSTNrO"
+        );
+        break;
+  
+      case "css":
+        addClass(
+          domElement,
+          "https://1drv.ms/b/s!AuDwzFWCb33QgrhHTvdSpkZX_mPAZg"
+        );
+        break;
+  
+      case "flexbox":
+        addClass(
+          domElement,
+          "https://1drv.ms/b/s!AuDwzFWCb33QgrZ2oXo3CEd7baoXaA"
+        );
+        break;
+  
+      case "grid":
+        addClass(
+          domElement,
+          "https://1drv.ms/b/s!AuDwzFWCb33QgrZ3VyRc-Nx1bmZbJw"
+        );
+        break;
+  
+      case "html":
+        addClass(
+          domElement,
+          "https://1drv.ms/b/s!AuDwzFWCb33QgrZ5hyB_K5pmpvfpvg"
+        );
+        break;
+  
+      case "git":
+        addClass(
+          domElement,
+          "https://1drv.ms/u/s!AuDwzFWCb33QgrZ6S-2pxUnrgoyQqw?e=y1AGlY"
+        );
+        break;
+  
+      case "linux":
+        addClass(
+          domElement,
+          "https://1drv.ms/b/s!AuDwzFWCb33QgrcBt2ScDJGbd-z_Kw?e=tdbukp"
+        );
+        break;
+  
+      case "cli":
+        addClass(
+          domElement,
+          "https://1drv.ms/x/s!AuDwzFWCb33QgrZ-JXPR5iHRccVIRA?e=j2EabM"
+        );
+        break;
+  
+      case "vscode":
+        addClass(
+          domElement,
+          "https://1drv.ms/b/s!AuDwzFWCb33QgrcDkk_sgui_0d7aZA?e=wgewBr"
+        );
+        break;
+  
+      case "adobexd":
+        addClass(
+          domElement,
+          "https://1drv.ms/u/s!AuDwzFWCb33QgrcFbSjDzWh59zgmdQ?e=IjU0Ky"
+        );
+        break;
+  
+      default:
+        break;
+    }
+  });
+}
 
-    case "tetris":
-      addClass(domElement, "https://sargentogato.github.io/Tetris-sg/");
-      break;
-
-    case "crazyQuote":
-      addClass(
-        domElement,
-        "https://sargentogato.github.io/crazyquotegenerator/"
-      );
-      break;
-
-    case "js":
-      addClass(
-        domElement,
-        "https://1drv.ms/x/s!AuDwzFWCb33QgrZtIVvj3SoU3cNlRg?e=TSTNrO"
-      );
-      break;
-
-    case "css":
-      addClass(
-        domElement,
-        "https://1drv.ms/b/s!AuDwzFWCb33QgrhHTvdSpkZX_mPAZg"
-      );
-      break;
-
-    case "flexbox":
-      addClass(
-        domElement,
-        "https://1drv.ms/b/s!AuDwzFWCb33QgrZ2oXo3CEd7baoXaA"
-      );
-      break;
-
-    case "grid":
-      addClass(
-        domElement,
-        "https://1drv.ms/b/s!AuDwzFWCb33QgrZ3VyRc-Nx1bmZbJw"
-      );
-      break;
-
-    case "html":
-      addClass(
-        domElement,
-        "https://1drv.ms/b/s!AuDwzFWCb33QgrZ5hyB_K5pmpvfpvg"
-      );
-      break;
-
-    case "git":
-      addClass(
-        domElement,
-        "https://1drv.ms/u/s!AuDwzFWCb33QgrZ6S-2pxUnrgoyQqw?e=y1AGlY"
-      );
-      break;
-
-    case "linux":
-      addClass(
-        domElement,
-        "https://1drv.ms/b/s!AuDwzFWCb33QgrcBt2ScDJGbd-z_Kw?e=tdbukp"
-      );
-      break;
-
-    case "cli":
-      addClass(
-        domElement,
-        "https://1drv.ms/x/s!AuDwzFWCb33QgrZ-JXPR5iHRccVIRA?e=j2EabM"
-      );
-      break;
-
-    case "vscode":
-      addClass(
-        domElement,
-        "https://1drv.ms/b/s!AuDwzFWCb33QgrcDkk_sgui_0d7aZA?e=wgewBr"
-      );
-      break;
-
-    case "adobexd":
-      addClass(
-        domElement,
-        "https://1drv.ms/u/s!AuDwzFWCb33QgrcFbSjDzWh59zgmdQ?e=IjU0Ky"
-      );
-      break;
-
-    default:
-      break;
-  }
-});
