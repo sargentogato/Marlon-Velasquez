4045a1e18f4bed1e9196057f7908574f3ea02821
Marlon Velasquez <marlonjvelasquez@gmail.com>
2020-08-30 18:04:32 +0200

    Refactoring -Added an if statemen to turn off some visual effects on screens samaller than 700px

--- script/cssInserter.js
+++ script/cssInserter.js
@@ -1,17 +1,20 @@
 function addClass(domElement, link) {
   let getElement = document.getElementById(`${domElement}`);
-  getElement.classList.remove("zoomOut");
-  getElement.classList.add("zoomIn");
 
-  getElement.addEventListener("mouseleave", (event) => {
-    if (`${domElement} ` !== `${event.target.id}`) {
-      getElement.classList.remove("zoomIn");
-      getElement.classList.add("zoomOut");
-    }
-  });
-
-  $(`#${domElement}`).click(function () {
+  if(screen.width > 770){
+    getElement.classList.remove("zoomOut");
+    getElement.classList.add("zoomIn");
+  
+    getElement.addEventListener("mouseleave", (event) => {
+      if (`${domElement} ` !== `${event.target.id}`) {
+        getElement.classList.remove("zoomIn");
+        getElement.classList.add("zoomOut");
+      }
+    });
+  }
+  
+  $(`#${domElement}`).click(function (event) {
+    event.stopImmediatePropagation()
     window.open(link, "_blank");
-    stopPropagation();
   });
 }
